94f05442207a044157961355c796f255875b96af
AkyunaAkish <akyunaakish@gmail.com>
2015-09-11 12:11:59 -0600

    keep comments on update and make a show route for comments and delete comments on their show page

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var posts = db.get('posts');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   posts.find({}, function (err, docs) {
-    console.log(docs);
+    // console.log(docs);
     res.render('index', {posts: docs});
   })
 
@@ -30,9 +30,39 @@ router.post('/', function (req, res, next) {
   }
 });
 
+
+router.post('/posts/:postId/comments/:commentId/delete', function(req,res,next){
+  posts.findOne({_id: req.params.postId}, function(err,doc){
+    var a = req.url.split('/');
+    var b = a.length -2;
+
+    var idFilter = doc.comments.filter(function(comment){
+      return comment.commentId != a[b];
+    })
+
+    doc.comments = idFilter;
+    posts.update({_id: req.params.postId}, doc, function(err, doc) {
+      res.redirect('/posts/' + req.params.postId)
+    })
+  })
+})
+
+router.get('/posts/:id/comment/:commentId', function(req,res,next){
+  var a = req.url.split('/');
+  var b = a.length -1;
+  posts.findOne({_id: req.params.id}, function (err, doc) {
+    var idFilter = doc.comments.filter(function(comment){
+      return comment.commentId == a[b];
+    })
+    res.render('commentShow', {comment: idFilter[0], postId: doc._id});
+
+  })
+})
+
+
+
 router.get('/posts/:id', function (req, res, next) {
   posts.findOne({_id: req.params.id}, function (err, doc) {
-    console.log(doc)
     res.render('show', {posts: doc});
 
   })
@@ -50,7 +80,16 @@ router.get('/posts/:id/edit', function (req, res, next) {
 
 router.post('/posts/:id/comments', function(req,res,next){
   posts.findOne({_id: req.params.id}, function(err,doc){
-    doc.comments.push(req.body.comment)
+    var idCount = 0;
+    if(doc.comments.length < 1){
+      idCount = 1;
+      doc.comments.push({commentId: idCount, comment: req.body.comment})
+    }else{
+      var lastIndex = doc.comments.length - 1;
+      var lastItem = doc.comments[lastIndex]
+      idCount = lastItem.commentId + 1;
+      doc.comments.push({commentId: idCount, comment: req.body.comment})
+    }
     posts.update({_id: req.params.id}, doc, function (err, doc) {
 
       if (err) throw err;
@@ -59,24 +98,31 @@ router.post('/posts/:id/comments', function(req,res,next){
   })
 })
 
-router.post('/posts/:id/comments/delete', function(req,res,next){
-  posts.findOne({_id: req.params.id}, function(err,doc){
-    var index = doc.comments.indexOf(req.body.comment)
-    doc.comments.splice(index,1);
-    posts.update({_id: req.params.id}, doc, function(err, doc) {
-      res.redirect('/posts/' + req.params.id)
-    })
-  })
-})
+
+
+
+
+
+// router.post('/posts/:id/comments/delete', function(req,res,next){
+//   posts.findOne({_id: req.params.id}, function(err,doc){
+//     var index = doc.comments.indexOf(req.body.comment)
+//     doc.comments.splice(index,1);
+//     posts.update({_id: req.params.id}, doc, function(err, doc) {
+//       res.redirect('/posts/' + req.params.id)
+//     })
+//   })
+// })
 
 router.post('/posts/:id/update', function (req, res, next) {
 
-  posts.update({_id: req.params.id}, req.body, function (err, doc) {
+  posts.findOne({_id: req.params.id}, function(err,doc){
+    doc.posts = req.body.posts;
+    posts.update({_id: req.params.id}, doc, function (err, doc) {
 
-    if (err) throw err;
-    res.redirect('/')
+      if (err) throw err;
+      res.redirect('/')
+    })
   })
-
 
 });
 
